7e46200f8ff4935c7269550ae1482c394c3087c7
= <=gaaraxb123@gmail.com>
2019-08-29 15:35:15 +0700

    add file

---
+++ ThucHanh02.java
@@ -0,0 +1,17 @@
+public class ThucHanh02 {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 25.36;
+        char c = 'b';
+        boolean b = true;
+        String str = "Hà Nội";
+
+        System.out.println("i = "+i);
+        System.out.println("f = "+f);
+        System.out.println("d = "+d);
+        System.out.println("c = "+c);
+        System.out.println("b = "+b);
+        System.out.println("str = "+str);
+    }
+}
